c776824d8f12cc605b24218a750c13360c51dd8e
jgp <jgp@cscs.ch>
2021-03-14 20:48:23 +0100

    valgrind4hpc

--- reframechecks/debug/cray_valgrind4hpc.py
+++ reframechecks/debug/cray_valgrind4hpc.py
@@ -1,4 +1,4 @@
-# Copyright 2019-2020 Swiss National Supercomputing Centre (CSCS/ETH Zurich)
+# Copyright 2019-2021 Swiss National Supercomputing Centre (CSCS/ETH Zurich)
 # HPCTools Project Developers. See the top-level LICENSE file for details.
 #
 # SPDX-License-Identifier: BSD-3-Clause
@@ -9,22 +9,14 @@ import reframe as rfm
 import reframe.utility.sanity as sn
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),
                 '../common')))  # noqa: E402
-import sphexa.sanity as sphs
-import sphexa.sanity_valgrind as sphsvalgrind
+import sphexa.hooks as hooks
 from reframe.core.backends import getlauncher
 
 
-# NOTE: jenkins restricted to 1 cnode
-mpi_tasks = [12]
-cubeside_dict = {1: 10, 12: 20, 24: 30}
-steps_dict = {1: 0, 12: 0, 24: 0}
-
-
 # {{{ class SphExaValgrind4hpcCheck
-@rfm.parameterized_test(*[[mpi_task]
-                          for mpi_task in mpi_tasks
-                          ])
-class SphExaValgrind4hpcCheck(rfm.RegressionTest):
+@rfm.simple_test
+class SphExa_Valgrind4hpc_Check(rfm.RegressionTest, hooks.setup_pe,
+                                hooks.setup_code):
     # {{{
     '''
     This class runs the test code with Cray valgrind4hpc
@@ -54,88 +46,66 @@ class SphExaValgrind4hpcCheck(rfm.RegressionTest):
     : 1112806.1    cti_be_da+        1         61  COMPLETED
     '''
     # }}}
+    steps = parameter([0])  # overhead is high -> 0 is recommended
+    compute_node = parameter([1])
+    np_per_c = parameter([1e2])  # overhead is high -> small is recommended
 
-    def __init__(self, mpi_task):
-        # super().__init__()
+    def __init__(self):
         # {{{ pe
         self.descr = 'Tool validation'
-        self.valid_prog_environs = ['PrgEnv-gnu', 'PrgEnv-intel', 'PrgEnv-pgi',
-                                    'PrgEnv-cray']
-        # self.valid_systems = ['daint:gpu', 'dom:gpu']
-        self.valid_systems = ['*']
+        self.valid_prog_environs = [
+            'PrgEnv-gnu', 'cpeGNU'
+            # 'PrgEnv-gnu', 'PrgEnv-intel', 'PrgEnv-pgi', 'PrgEnv-cray',
+            # 'PrgEnv-aocc', 'cpeGNU', 'cpeIntel', 'cpeCray', 'cpeAMD',
+        ]
+        # NOTE: dom (slurm/20.11.4) is failing
+        self.valid_systems = [
+            'dom:mc', 'dom:gpu', 'daint:mc', 'daint:gpu',
+            'eiger:mc', 'pilatus:mc'
+        ]
+        self.tool = 'valgrind4hpc'
+        self.modules = [self.tool]
         self.maintainers = ['JG']
-        self.tags = {'sph', 'hpctools', 'cpu'}
+        self.tags = {'sph', 'hpctools', 'cpu', 'craype', 'debugging'}
         # }}}
 
         # {{{ compile
-        self.testname = 'sqpatch'
-        self.tool = 'valgrind4hpc'
-        self.prgenv_flags = {
-            'PrgEnv-gnu': ['-I.', '-I./include', '-std=c++14', '-g', '-O0',
-                           '-DUSE_MPI', '-DNDEBUG'],
-            'PrgEnv-intel': ['-I.', '-I./include', '-std=c++14', '-g', '-O0',
-                             '-DUSE_MPI', '-DNDEBUG'],
-            'PrgEnv-cray': ['-I.', '-I./include', '-std=c++17', '-g', '-O0',
-                            '-DUSE_MPI', '-DNDEBUG'],
-            'PrgEnv-pgi': ['-I.', '-I./include', '-std=c++14', '-g', '-O0',
-                           '-DUSE_MPI', '-DNDEBUG'],
-        }
-        tc_ver = '20.08'
-        self.prebuild_cmds = ['module rm xalt', 'module list -t']
-        self.tool_modules = {
-            'PrgEnv-gnu': [f'CrayGNU/.{tc_ver}', self.tool],
-            'PrgEnv-intel': [f'CrayIntel/.{tc_ver}', self.tool],
-            'PrgEnv-cray': [f'CrayCCE/.{tc_ver}', self.tool],
-            'PrgEnv-pgi': [f'CrayPGI/.{tc_ver}', self.tool],
-        }
-        self.build_system = 'SingleSource'
+        self.testname = 'sedov'
         self.sourcepath = f'{self.testname}.cpp'
+        # TODO: self.prgenv_flags = -O0
         self.executable = self.tool
-        self.target_executable = f'./{self.testname}.exe'
-        self.postbuild_cmds = [
-            f'mv {self.executable} {self.target_executable}',
-        ]
+        self.target_executable = './mpi+omp'
+        # self.sourcepath = f'{self.testname}.cpp'
+        # self.target_executable = f'./{self.testname}.exe'
+        # self.postbuild_cmds = [f'mv {self.tool} {self.target_executable}']
         # }}}
 
         # {{{ run
-        ompthread = 1
-        self.num_tasks = mpi_task
-        self.cubeside = cubeside_dict[mpi_task]
-        self.steps = steps_dict[mpi_task]
-        self.name = 'sphexa_valgrind4hpc_{}_{:03d}mpi_{:03d}omp_{}n_{}steps'. \
-            format(self.testname, mpi_task, ompthread, self.cubeside,
-                   self.steps)
-        self.num_tasks_per_node = 12
-        self.num_cpus_per_task = ompthread
-        self.num_tasks_per_core = 1
-        self.use_multithreading = False
-        self.exclusive = True
         self.time_limit = '10m'
         self.variables = {
-            'CRAYPE_LINK_TYPE': 'dynamic',
-            'OMP_NUM_THREADS': str(self.num_cpus_per_task),
             'PKG_CONFIG_PATH':
                 '$VALGRIND4HPC_INSTALL_DIR/lib/pkgconfig:$PKG_CONFIG_PATH',
         }
-        self.tool_opts = (
-            f' -n{self.num_tasks}'
-            # f' --launcher-args=""'
-            f' --valgrind-args="--track-origins=yes --leak-check=full"'
-        )
-        # valgrind4hpc -n32 --launcher-args="-N16 -j2"
-        # --valgrind-args="--track-origins=yes --leak-check=full" ./a.out
-        # -- arg1 arg2
-        self.executable_opts = [
-            self.tool_opts, self.target_executable,
-            f'-- -n {self.cubeside}', f'-s {self.steps}', '2>&1']
         self.version_rpt = 'version.rpt'
         self.which_rpt = 'which.rpt'
         self.prerun_cmds = [
-            'module rm xalt',
-            f'echo $VALGRIND4HPC_VERSION > {self.version_rpt}',
+            f'which {self.tool} &> {self.which_rpt}',
+            f'srun --version > {self.version_rpt}',
+            # f'{self.tool} --version >> {self.version_rpt}',
+            f'pkg-config --variable=prefix valgrind >> {self.version_rpt}',
+            f'echo VALGRIND4HPC_L=$VALGRIND4HPC_LEVEL >> {self.version_rpt}',
             f'grep PACKAGE_VERSION $VALGRIND4HPC_INSTALL_DIR/include/'
             f'valgrind/config.h >> {self.version_rpt}',
-            f'which {self.tool} &> {self.which_rpt}',
+        ]
+
+        self.tool_opts = (
+            f' -n{self.num_tasks}'
+            f' --valgrind-args="--track-origins=yes --leak-check=full"'
+            # f' --from-ranks=0-{self.num_tasks-1} '
+            # f' --launcher-args=""'
+        )
+        self.executable_opts = [
+            self.tool_opts, self.target_executable, '--',
         ]
         # }}}
 
@@ -146,45 +116,39 @@ class SphExaValgrind4hpcCheck(rfm.RegressionTest):
         self.sanity_patterns = sn.all([
             # check the job output:
             sn.assert_found(r'Total time for iteration\(0\)', self.stdout),
-            sn.assert_found(sanity_1, self.stdout),
-            sn.assert_found(sanity_2, self.stdout),
-            sn.assert_found(sanity_3, self.stdout),
+            sn.assert_not_found(sanity_1, self.stdout),
+            sn.assert_not_found(sanity_2, self.stdout),
+            # sn.assert_found(sanity_3, self.stdout),
         ])
         # }}}
 
         # {{{ performance
-        # {{{ internal timers
-        self.time_rpt = 'time.rpt'
         # the tool flushes stdout hence we need this trick:
+        self.time_rpt = 'time.rpt'
         self.prerun_cmds += [f'echo starttime=`date +%s` > {self.time_rpt}']
         self.postrun_cmds += [
             f'echo stoptime=`date +%s` >> {self.time_rpt}',
-            f'cat {self.time_rpt}']
+        ]
+        # see common/sphexa/hooks.py
         # }}}
-
-        # {{{ perf_patterns:
-        basic_perf_patterns = sn.evaluate(sphs.basic_perf_patterns(self))
-        tool_perf_patterns = sn.evaluate(sphsvalgrind.vhpc_perf_patterns(self))
-        self.perf_patterns = {**basic_perf_patterns, **tool_perf_patterns}
-        # }}}
-
-        # {{{ reference:
-        self.reference = sn.evaluate(sphs.basic_reference_scoped_d(self))
-        self.reference = sn.evaluate(sphsvalgrind.vhpc_tool_reference(self))
-        # }}}
-        # }}}
+#         # {{{ perf_patterns:
+#         basic_perf_patterns = sn.evaluate(sphs.basic_perf_patterns(self))
+#         tool_perf_patterns = \
+#           sn.evaluate(sphsvalgrind.vhpc_perf_patterns(self))
+#         self.perf_patterns = {**basic_perf_patterns, **tool_perf_patterns}
+#         # }}}
+#         # {{{ reference:
+#         self.reference = sn.evaluate(sphs.basic_reference_scoped_d(self))
+#         self.reference = sn.evaluate(sphsvalgrind.vhpc_tool_reference(self))
+#         # }}}
 
     # {{{ hooks
-    @rfm.run_before('compile')
-    def set_compiler_flags(self):
-        self.modules = self.tool_modules[self.current_environ.name]
-        self.build_system.cxxflags = \
-            self.prgenv_flags[self.current_environ.name]
-
+    # {{{ set_launcher
     @rfm.run_before('run')
     def set_launcher(self):
         # The job launcher has to be changed because
-        # valgrind4hpc can not be called with srun
+        # the tool can be called without srun
         self.job.launcher = getlauncher('local')()
+    # }}}
     # }}}
 # }}}
